b1bf78d458138eb0ac4778ea903efc70fc0ec2ee
scriptjunkie <scriptjunkie@scriptjunkie.us>
2017-09-23 15:15:21 -0500

    Initial commit - support for RSA keys

---
+++ README.rst
@@ -0,0 +1,4 @@
+puttykeys
+=========
+
+A library to convert SSH keys from the Putty format to the OpenSSH format.
---
+++ setup.py
@@ -0,0 +1,25 @@
+from setuptools import setup
+import os
+
+setup(
+    name='puttykeys',
+    version='1.0.0',
+    description='Library to convert Putty private keys to OpenSSH format',
+    author='Matt Weeks',
+    author_email='scriptjunkie@scriptjunkie.us',
+    license='MIT',
+    url='https://github.com/scriptjunkie/puttykeys.git',
+    packages=['puttykeys'],
+    install_requires=['cryptography'],
+    platforms = ["POSIX", "Windows"],
+    provides = ["puttykeys"],
+    keywords='putty development',
+    long_description = open(os.path.join(os.path.dirname(__file__), "README.rst"), "r").read(),
+    classifiers=[
+        'Intended Audience :: Developers',
+        'Topic :: Software Development :: Libraries :: Python Modules',
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: MIT License'
+    ]
+)
